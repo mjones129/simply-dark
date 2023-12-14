1ea69faaab426870d0d8433a975d101188da5cba
Matt Jones <29353833+mjones129@users.noreply.github.com>
2023-12-14 18:57:31 -0500

    add top level options page from docs

--- simply-dark.php
+++ simply-dark.php
@@ -7,38 +7,7 @@
 *
  */
 
- function sd_options_page_html() {
-	// check user capabilities
-	if ( ! current_user_can( 'manage_options' ) ) {
-		return;
-	}
-	?>
-	<div class="wrap">
-		<h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
-		<form action="options.php" method="post">
-			<?php
-			// output security fields for the registered setting "wporg_options"
-			settings_fields( 'sd_options' );
-			// output setting sections and their fields
-			// (sections are registered for "wporg", each field is registered to a specific section)
-			do_settings_sections( 'simply-dark' );
-			// output save settings button
-			submit_button( __( 'Save Settings', 'textdomain' ) );
-			?>
-		</form>
-	</div>
-	<?php
-}
 
-
-function sd_init() {
-   register_post_type('labs', [
-      'label' => 'Labs',
-      // .. ect
-      'menu_icon' => 'data:image/svg+xml;base64,' . base64_encode('<svg width="20" height="20" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path fill="black" d="M1591 1448q56 89 21.5 152.5t-140.5 63.5h-1152q-106 0-140.5-63.5t21.5-152.5l503-793v-399h-64q-26 0-45-19t-19-45 19-45 45-19h512q26 0 45 19t19 45-19 45-45 19h-64v399zm-779-725l-272 429h712l-272-429-20-31v-436h-128v436z"/></svg>')
-   ]);
-}
-add_action('init', 'sd_init');
 
 //Activate Plugin
 function sd_activate() { 
@@ -50,18 +19,6 @@ function sd_activate() {
 register_activation_hook( __FILE__, 'sd_activate' );
 
 
-function sd_options_page()
-{
-	add_submenu_page(
-		'options-general.php',
-		'Simply Dark',
-		'Simply Dark',
-		'manage_options',
-		'simply-dark',
-		'sd_options_page_html'
-	);
-}
-add_action('admin_menu', 'sd_options_page');
 
  function sd_kill_the_lights() {
     wp_register_style('simply-dark', plugin_dir_url(__FILE__) . 'dark.css', array(), '1.0', 'all');
@@ -70,3 +27,152 @@ add_action('admin_menu', 'sd_options_page');
 
  add_action('admin_enqueue_scripts', 'sd_kill_the_lights');
 
+/**
+ * @internal never define functions inside callbacks.
+ * these functions could be run multiple times; this would result in a fatal error.
+ */
+
+/**
+ * custom option and settings
+ */
+function wporg_settings_init() {
+	// Register a new setting for "wporg" page.
+	register_setting( 'wporg', 'wporg_options' );
+
+	// Register a new section in the "wporg" page.
+	add_settings_section(
+		'wporg_section_developers',
+		__( 'The Matrix has you.', 'wporg' ), 'wporg_section_developers_callback',
+		'wporg'
+	);
+
+	// Register a new field in the "wporg_section_developers" section, inside the "wporg" page.
+	add_settings_field(
+		'wporg_field_pill', // As of WP 4.6 this value is used only internally.
+		                        // Use $args' label_for to populate the id inside the callback.
+			__( 'Pill', 'wporg' ),
+		'wporg_field_pill_cb',
+		'wporg',
+		'wporg_section_developers',
+		array(
+			'label_for'         => 'wporg_field_pill',
+			'class'             => 'wporg_row',
+			'wporg_custom_data' => 'custom',
+		)
+	);
+}
+
+/**
+ * Register our wporg_settings_init to the admin_init action hook.
+ */
+add_action( 'admin_init', 'wporg_settings_init' );
+
+
+/**
+ * Custom option and settings:
+ *  - callback functions
+ */
+
+
+/**
+ * Developers section callback function.
+ *
+ * @param array $args  The settings array, defining title, id, callback.
+ */
+function wporg_section_developers_callback( $args ) {
+	?>
+	<p id="<?php echo esc_attr( $args['id'] ); ?>"><?php esc_html_e( 'Follow the white rabbit.', 'wporg' ); ?></p>
+	<?php
+}
+
+/**
+ * Pill field callbakc function.
+ *
+ * WordPress has magic interaction with the following keys: label_for, class.
+ * - the "label_for" key value is used for the "for" attribute of the <label>.
+ * - the "class" key value is used for the "class" attribute of the <tr> containing the field.
+ * Note: you can add custom key value pairs to be used inside your callbacks.
+ *
+ * @param array $args
+ */
+function wporg_field_pill_cb( $args ) {
+	// Get the value of the setting we've registered with register_setting()
+	$options = get_option( 'wporg_options' );
+	?>
+	<select
+			id="<?php echo esc_attr( $args['label_for'] ); ?>"
+			data-custom="<?php echo esc_attr( $args['wporg_custom_data'] ); ?>"
+			name="wporg_options[<?php echo esc_attr( $args['label_for'] ); ?>]">
+		<option value="red" <?php echo isset( $options[ $args['label_for'] ] ) ? ( selected( $options[ $args['label_for'] ], 'red', false ) ) : ( '' ); ?>>
+			<?php esc_html_e( 'red pill', 'wporg' ); ?>
+		</option>
+ 		<option value="blue" <?php echo isset( $options[ $args['label_for'] ] ) ? ( selected( $options[ $args['label_for'] ], 'blue', false ) ) : ( '' ); ?>>
+			<?php esc_html_e( 'blue pill', 'wporg' ); ?>
+		</option>
+	</select>
+	<p class="description">
+		<?php esc_html_e( 'You take the blue pill and the story ends. You wake in your bed and you believe whatever you want to believe.', 'wporg' ); ?>
+	</p>
+	<p class="description">
+		<?php esc_html_e( 'You take the red pill and you stay in Wonderland and I show you how deep the rabbit-hole goes.', 'wporg' ); ?>
+	</p>
+	<?php
+}
+
+/**
+ * Add the top level menu page.
+ */
+function wporg_options_page() {
+	add_menu_page(
+		'WPOrg',
+		'WPOrg Options',
+		'manage_options',
+		'wporg',
+		'wporg_options_page_html'
+	);
+}
+
+
+/**
+ * Register our wporg_options_page to the admin_menu action hook.
+ */
+add_action( 'admin_menu', 'wporg_options_page' );
+
+
+/**
+ * Top level menu callback function
+ */
+function wporg_options_page_html() {
+	// check user capabilities
+	if ( ! current_user_can( 'manage_options' ) ) {
+		return;
+	}
+
+	// add error/update messages
+
+	// check if the user have submitted the settings
+	// WordPress will add the "settings-updated" $_GET parameter to the url
+	if ( isset( $_GET['settings-updated'] ) ) {
+		// add settings saved message with the class of "updated"
+		add_settings_error( 'wporg_messages', 'wporg_message', __( 'Settings Saved', 'wporg' ), 'updated' );
+	}
+
+	// show error/update messages
+	settings_errors( 'wporg_messages' );
+	?>
+	<div class="wrap">
+		<h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
+		<form action="options.php" method="post">
+			<?php
+			// output security fields for the registered setting "wporg"
+			settings_fields( 'wporg' );
+			// output setting sections and their fields
+			// (sections are registered for "wporg", each field is registered to a specific section)
+			do_settings_sections( 'wporg' );
+			// output save settings button
+			submit_button( 'Save Settings' );
+			?>
+		</form>
+	</div>
+	<?php
+}
